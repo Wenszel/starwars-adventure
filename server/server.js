3c307d705f152b28e8529513a2b5837e20cb3189
Wenszel <wiktor.smaga13@gmail.com>
2021-06-14 15:55:30 +0200

    added socketio

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const socket = require("socket.io");
+const path = require('path');
 const app = express();
+app.use(express.static("public"));
 const PORT = 3000 || env.PORT;
-app.listen(PORT, () => { 
+const server = app.listen(PORT, () => { 
     console.log("server running on port: " + PORT);
+});
+app.get("/", (req, res) => res.sendFile(path.join(__dirname,"/public/index.html")))
+const io = socket(server);
+io.on("connection", function (socket) {
+    console.log("Made socket connection");
 });
