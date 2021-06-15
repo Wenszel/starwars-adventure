7268084b5c0bd273046a818b96724dbdc6d0556b
Wenszel <wiktor.smaga13@gmail.com>
2021-06-15 19:46:25 +0200

    added socket rooms

--- server/public/index.js
+++ server/public/index.js
@@ -1,1 +1,5 @@
 const socket = io();
+socket.on('connectToRoom',function(data) {
+    document.body.innerHTML = '';
+    document.write(data);
+ });
--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 const express = require('express');
-const socket = require("socket.io");
-const path = require('path');
 const app = express();
+
+const path = require('path');
 app.use(express.static("public"));
 const PORT = 3000 || env.PORT;
 const server = app.listen(PORT, () => { 
     console.log("server running on port: " + PORT);
 });
 app.get("/", (req, res) => res.sendFile(path.join(__dirname,"/public/index.html")))
-const io = socket(server);
+const io = require('socket.io')(server);
+let roomNumber = 1;
 io.on("connection", function (socket) {
-    console.log("Made socket connection");
+    const room = io.sockets.adapter.rooms.get(`room-${roomNumber}`)?.size;
+    if(room > 1) roomNumber++;
+    socket.join(`room-${roomNumber}`);
+    //Send this event to everyone in the room.
+    io.sockets.in(`room-${roomNumber}`).emit('connectToRoom', `You are in room no. ${roomNumber}`);
 });
+
 // MongoDB
 const mongoose = require('mongoose');
 const CONNECTION_URI =  require('./credentials.js');
